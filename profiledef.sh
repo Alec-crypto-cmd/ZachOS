6420c18b1d60bc23c69c0ddb081ae7e6deeb2626
Alec <workplace0303@outlook.com>
2026-09-18 19:26:43 +0200

    Use current UEFI systemd-boot archiso mode

--- profiledef.sh
+++ profiledef.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 iso_name="zachos"
-iso_label="ZACHOS_$(date +%Y%m)"
+iso_label="ZACHOS_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Zach Tech Turf"
 iso_application="ZachOS Arch Linux"
-iso_version="$(date +%Y.%m.%d)"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
-buildmodes=("iso")
-bootmodes=("bios.syslinux" "uefi-x64.systemd-boot")
+# UEFI is intentional: systemd-boot requires UEFI and replaces the removed legacy syslinux mode.
+bootmodes=('uefi.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
